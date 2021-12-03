663320da6d2b6ec0e4acb4eb97cbc8be8d6d1fcc
Ross Couldrey <38637723+rosscouldrey@users.noreply.github.com>
2021-12-03 15:14:41 -0500

    Update NewFactSalesView.sql

--- NewFactSalesView.sql
+++ NewFactSalesView.sql
@@ -4,11 +4,13 @@ GO
 CREATE VIEW LDW.vwFactSales
 AS
 SELECT *,
-CAST(fct.filepath(3) AS DATE) AS SalesOrderPathDate
+--updated filepath parameter to 2
+CAST(fct.filepath(2) AS DATE) AS SalesOrderPathDate
  FROM 
 OPENROWSET 
 (
-    BULK 'conformed/facts/factsales/*/*/*/*.parquet',
+--updated BULK path to remove 1 layer of folder structure
+    BULK 'conformed/facts/factsales/*/*/*.parquet',
     DATA_SOURCE = 'ExternalDataSourceDataLake',
     FORMAT = 'Parquet'
 ) AS fct
